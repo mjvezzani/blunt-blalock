8313b40d0e1e44ce5df734d7a501067420cf8465
Michael Vezzani <mjvezzani@gmail.com>
2013-05-22 15:20:19 -0400

    Create initial Rails project.

---
+++ qotd/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qotd::Application.config.secret_token = 'e6bc3fe00daa79fef008bb300afaa938936ae35220cd75aa25f881ed6250e1b612ddefda3b88c64b87cf141997d60df18ab972b5a1a8505e1b0d6d953d7bc8de'
